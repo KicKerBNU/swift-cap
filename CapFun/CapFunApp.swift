f40b55f9e234e491721e6a8c87bd34e40573e013
Everton Buzzi <evertonbuzzi@gmail.com>
2023-10-08 10:33:07 +0100

    Initial Commit

---
+++ CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by Everton Buzzi on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
